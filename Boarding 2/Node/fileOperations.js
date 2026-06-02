7cd8b5dc88dfbb1861147cccb40c9ac0c029d06d
Haranck <harankrishna03@gmail.com>
2026-06-02 12:00:56 +0530

    Refactor code structure for improved readability and maintainability

--- Boarding 2/Node/fileOperations.js
+++ Boarding 2/Node/fileOperations.js
@@ -1,1 +1,53 @@
+const fs  = require('fs')
 
+fs.readFile('input.txt','utf-8',(err,data)=>{
+    if(err){
+        throw new Error(err)
+    }
+    console.log(data)
+})
+
+fs.writeFile('input.txt','Heyy Dear',(err)=>{
+    if(err){
+        throw new Error
+    }
+    console.log('file writed successfully')
+}) 
+// fs.writeFile('demo.txt','demo file',(err)=>{
+//     if(err){
+//         throw new Error
+//     }
+//     console.log('file writed successfully')
+// }) 
+
+fs.appendFile('input.txt',' Love you',(err)=>{
+    if(err){
+        throw new Error(err)
+    }
+    console.log('file appended')
+})
+
+// if(fs.existsSync('input.txt')){
+//     console.log('file exists')
+// }else{
+//     console.log('file not exist')
+// }
+
+// fs.rename('output.txt','input.txt',(err)=>{
+//     if(err){
+//         throw new Error(err)
+//     }
+//     console.log('file renamed')
+// })
+
+// fs.unlink('demo.txt',(err)=>{
+//     if(err){
+//         throw new Error(err)
+//     }
+//     console.log('file deleted successfully')
+// })
+
+fs.link('input.txt','demo.txt',(err)=>{
+    if(err) throw new Error(err)
+    console.log("hard link created successfully")
+})
