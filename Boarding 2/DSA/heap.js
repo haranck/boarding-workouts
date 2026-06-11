e32517614040f8b7d8df3f2c0438d38f0f462205
Haranck <harankrishna03@gmail.com>
2026-06-11 20:58:27 +0530

    Implement buildHeap method in MinHeap and MaxHeap classes; enhance heapifyDown and print methods for clarity

--- Boarding 2/DSA/heap.js
+++ Boarding 2/DSA/heap.js
@@ -4,6 +4,12 @@ class MinHeap {
     constructor() {
         this.heap = [];
     }
+    buildHeap(arr) {
+        this.heap = arr;
+        for (let i = Math.floor(this.heap.length / 2); i >= 0; i--) {
+            this.heapifyDown(i);
+        }
+    }
     insert(value) {
         this.heap.push(value);
         this.heapifyUp(this.heap.length - 1);
@@ -60,13 +66,14 @@ class MinHeap {
 }
 
 const min = new MinHeap();
-
-min.insert(10);
-min.insert(50);
-min.insert(60);
-min.insert(30);
-min.insert(5);
-min.remove();
+const array = [2, 4, 1, 5, 3, 2];
+min.buildHeap(array);
+// min.insert(10);
+// min.insert(50);
+// min.insert(60);
+// min.insert(30);
+// min.insert(5);
+// min.remove();
 min.print();
 
 //////////////////////////////////////////////////////////////////////
@@ -76,6 +83,12 @@ min.print();
 class MaxHeap {
     constructor() {
         this.heap = [];
+    }
+    buildHeap(arr) {
+        this.heap = arr;
+        for (let i = Math.floor(this.heap.length / 2); i >= 0; i--) {
+            this.heapifyDown(i);
+        }
     }
     insert(value) {
         this.heap.push(value);
@@ -104,34 +117,75 @@ class MaxHeap {
             parent = Math.floor((index - 1) / 2);
         }
     }
-	heapifyDown(index){
-		let largest = index
-		let left = 2 * index + 1
-		let right = 2 * index + 2
+    heapifyDown(index) {
+        let largest = index;
+        let left = 2 * index + 1;
+        let right = 2 * index + 2;
 
-		if(left < this.heap.length && this.heap[largest] < this.heap[left]){
-			largest = left
-		}
-		if(right < this.heap.length && this.heap[largest] < this.heap[right]){
-			largest = right
-		}
+        if (left < this.heap.length && this.heap[largest] < this.heap[left]) {
+            largest = left;
+        }
+        if (right < this.heap.length && this.heap[largest] < this.heap[right]) {
+            largest = right;
+        }
 
-		if(largest !== index){
-			[this.heap[index],this.heap[largest]] = [this.heap[largest],this.heap[index]]
-			this.heapifyDown(largest)
-		}
-	}
-	print(){
-		console.log('max Heap ',this.heap)
-	}
+        if (largest !== index) {
+            [this.heap[index], this.heap[largest]] = [
+                this.heap[largest],
+                this.heap[index],
+            ];
+            this.heapifyDown(largest);
+        }
+    }
+    print() {
+        console.log("max Heap ", this.heap);
+    }
 }
 
-const max = new MaxHeap()
-max.insert(10)
-max.insert(20)
-max.insert(50)
-max.insert(5)
-max.insert(9)
-max.remove()
+const max = new MaxHeap();
+const arrayy = [1, 2, 4, 5, 3, 2];
+max.buildHeap(arrayy);
+// max.insert(10);
+// max.insert(20);
+// max.insert(50);
+// max.insert(5);
+// max.insert(9);
+// max.remove()
 
-max.print()
+max.print();
+
+///////////////////////////////////////////////////////////////////////
+
+// heapsort
+
+function heapify(arr, n, index) {
+    let largest = index;
+    let left = 2 * index + 1;
+    let right = 2 * index + 2;
+
+    if (left < n && arr[largest] < arr[left]) {
+        largest = left;
+    }
+    if (right < n && arr[largest] < arr[right]) {
+        largest = right;
+    }
+    if (largest !== index) {
+        [arr[index], arr[largest]] = [arr[largest], arr[index]];
+        heapify(arr, n, largest);
+    }
+}
+
+function heapSort(arr) {
+    let n = arr.length;
+    for (let i = Math.floor(n / 2); i > 0; i--) {
+        heapify(arr, n, i);
+    }
+    for (let j = n - 1; j > 0; j--) {
+        [arr[0], arr[j]] = [arr[j], arr[0]];
+        heapify(arr, j, 0);
+    }
+    return arr;
+}
+
+const arr = [8,5,4,7,3,1,];
+console.log("heap sort ", heapSort(arr));
